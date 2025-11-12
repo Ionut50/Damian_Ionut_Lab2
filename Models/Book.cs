9a4446ef9e063b00af7d5f664c3724b2124cec1e
damian ionut <ionutdamian50@hotmail.com>
2025-11-12 16:20:19 +0200

    lab6

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,9 +8,13 @@ namespace Damian_Ionut_Lab2.Models
     {
         public int ID { get; set; }
         [Display(Name = "Book Title")]
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(150, MinimumLength = 3,
+        ErrorMessage = "Titlul trebuie sa aiba intre 3 si 150 caractere")]
         public string Title { get; set; }
-      
-        [Column(TypeName = "decimal(6, 2)")]
+
+        [Column(TypeName = "decimal(6, 2)")]    
+            [Range(0.01, 500)]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Damian_Ionut_Lab2.Data;
+using Damian_Ionut_Lab2.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Damian_Ionut_Lab2.Data;
-using Damian_Ionut_Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Damian_Ionut_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class CreateModel : BookCategoriesPageModel
     {
         private readonly Damian_Ionut_Lab2.Data.Damian_Ionut_Lab2Context _context;
